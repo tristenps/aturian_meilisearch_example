76ce56ff4295d3cb73923545c4c4bb87836a70e7
Tristen Pankake-Sieminski <tristenpsieminski@gmail.com>
2021-01-14 16:12:06 -0600

    Updated error-handler

--- src/middlewares.js
+++ src/middlewares.js
@@ -29,10 +29,18 @@ const notFound = (req, res, next) => {
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  // Error handling for Node, meili and Auth errors
+  // const statusCode = error.status ? error.status : (error.httpStatus ? error.httpStatus : 500) ;
+  let statusCode;
+  if (error.status) {
+    statusCode = error.status;
+  } else if (error.httpStatus) {
+    statusCode = error.httpStatus;
+  } else {
+    statusCode = 500;
+  }
   res.status(statusCode);
-  // eslint-disable-next-line no-console
-  console.log(error);
+  // console.log(error);
   res.json({
     message: error.message,
     // stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
